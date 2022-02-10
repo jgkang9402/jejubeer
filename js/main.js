43a77c437ec7db3c54a683025dc25a9cbb4164e0
jeonggu Kang <93317914+jgkang9402@users.noreply.github.com>
2022-02-10 20:15:42 +0900

    main

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let slideBox = document.querySelector(".slide_imgbox1")
+let slideList = document.querySelectorAll(".slide_imgbox1 li")
+let slideLength = slideList.length
+
+
+function slideWidth(){
+    
+}
